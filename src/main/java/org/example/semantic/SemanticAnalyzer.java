a66b5071fbcc0a6abae3fe08771eaf385d88049a
Jacob <Jkaae23@student.aau.dk>
2025-04-22 13:49:07 +0200

    base version

--- src/main/java/org/example/semantic/SemanticAnalyzer.java
+++ src/main/java/org/example/semantic/SemanticAnalyzer.java
@@ -6,66 +6,43 @@ import org.example.ast.*;
 public class SemanticAnalyzer {
     private final Map<String, TypeNode> types = new HashMap<>();
     private final Map<String, ActionNode> actions = new HashMap<>();
-    private final SymbolTable symbols = new SymbolTable();
+
+    // Hjælper funktion til at enter et symbol i et given hashmap.
+    private <T> void enterSymbol(Map<String, T> table, String name, T symbol, String kind) {
+        if (table.containsKey(name)) {
+            throw new SemanticException(kind + " duplicate: " + name);
+        }
+        table.put(name, symbol);
+    }
 
     public void analyze(DomainNode domain, ProblemNode problem) {
 
-        for (TypeNode type : domain.getTypes()) {
-            symbols.enterSymbol(type.getName(), new SymbolInfo(SymbolInfo.Kind.TYPE, type));
-            for (AttributeNode attribute : type.getAttributes()) {
-                symbols.enterSymbol(type.getName() + "." + attribute.getIdentifier(), new SymbolInfo(SymbolInfo.Kind.ATTRIBUTE, attribute));
-            }
-        }
-
-        for (ActionNode action : domain.getActions()) {
-            symbols.enterSymbol(action.getName(), new SymbolInfo(SymbolInfo.Kind.ACTION, action));
-        }
-
-
-        for (ObjectNode object : problem.getObjects()) {
-            String typeName = object.getType();
-            String arrayName = object.getArrayName();
-            String elementName = object.getElementName();
-
-            System.out.println(arrayName);
-
-            symbols.retrieveSymbol(typeName);
-
-            if (!symbols.isInCurrentScope(arrayName)) {
-                symbols.enterSymbol(arrayName, new SymbolInfo(SymbolInfo.Kind.ARRAY, typeName));
-            }
-
-            // robot robots[] = {r1, r2}; room rooms[] = {r1}; -> error
-            if (symbols.isInCurrentScope(elementName)) {
-                throw new SemanticException("Element '" + elementName + "' is already declared");
-            }
-            symbols.enterSymbol(elementName, new SymbolInfo(SymbolInfo.Kind.OBJECT, object));
-
-        }
-        
-        /* d
         // Tjek typer
         for (TypeNode type : domain.getTypes()) {
-            if (types.containsKey(type.getName())) {
-                throw new SemanticException("Type duplicate: " + type.getName());
-            } else {
-                types.put(type.getName(), type);
-            }
+            enterSymbol(types, type.getName(), type, "Type");
         }
 
         // Tjek actions
         for (ActionNode action : domain.getActions()) {
-            if (actions.containsKey(action.getName())) {
-                throw new SemanticException("Action duplicate: " + action.getName());
-            } else {
-                actions.put(action.getName(), action);
-            }
+            enterSymbol(actions, action.getName(), action, "Action");
         }
-         */
 
         // Tjek import af domain
         if (!domain.getName().equals(problem.getImportName())) {
             throw new SemanticException("Import name mismatch: " + problem.getImportName() + " Expected: " + domain.getName());
         }
+
+        // type check ide
+/*
+        checkProblem();
+        checkTypes();
+        checkAction();
+        checkStatement();
+        checkExpression();
+        checkObjects();
+        checkInitial();
+        checkGoal();
+
+ */
     }
 }
