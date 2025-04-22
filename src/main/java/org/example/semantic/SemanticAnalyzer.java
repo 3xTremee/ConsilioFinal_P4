da515c88fb2c2092ef6ec55c62ab840811f5bfbb
3xTremee <lasse.b.soerensen@gmail.com>
2025-04-22 13:59:31 +0200

    Opdateret med tjek attribute og helper

--- src/main/java/org/example/semantic/SemanticAnalyzer.java
+++ src/main/java/org/example/semantic/SemanticAnalyzer.java
@@ -32,6 +32,14 @@ public class SemanticAnalyzer {
             throw new SemanticException("Import name mismatch: " + problem.getImportName() + " Expected: " + domain.getName());
         }
 
+        // tjek fields (vi kalder det attributes) for type definitions.
+        for (TypeNode type : domain.getTypes()) {
+            for (AttributeNode attr : type.getAttributes()) {
+                checkValueNode(attr.getValue());
+            }
+        }
+
+
         // type check ide
 /*
         checkProblem();
@@ -44,5 +52,29 @@ public class SemanticAnalyzer {
         checkGoal();
 
  */
+    }
+
+    // Helper method which checks the value the fields of the types can take.
+    /* location: door || room, in this example, there needs to be a valid type definition named door and room for it
+    *  to pass the type check. */
+    private void checkValueNode(ValueNode value) {
+        if (value instanceof ValueTypeNode) {
+            String typeName = ((ValueTypeNode) value).getTypeName();
+            if (!(types.containsKey(typeName)
+                    || "int".equals(typeName)
+                    || "boolean".equals(typeName)))
+                throw new SemanticException("Unknown type in attribute: " + typeName);
+        } else if (value instanceof BaseValueNode) {
+            String typeName = ((BaseValueNode) value).getValue();
+            if (!(types.containsKey(typeName)
+                    || "int".equals(typeName)
+                    || "boolean".equals(typeName)))
+                throw new SemanticException("Unknown type in attribute: " + typeName);
+        } else if (value instanceof ArrayValueNode) {
+            checkValueNode(((ArrayValueNode) value).getElementType());
+        } else if (value instanceof OrValueNode) {
+            checkValueNode(((OrValueNode) value).getLeft());
+            checkValueNode(((OrValueNode) value).getRight());
+        }
     }
 }
